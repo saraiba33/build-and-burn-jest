621fddc092312b080c3de94e44669cdb7c834891
Sara Ibarra <saraiba33@gmail.com>
2022-05-03 20:54:07 -0600

    Remove solution

--- index.js
+++
@@ -1,7 +0,0 @@
-function multiple(numberOne, numberTwo) {
-    return numberOne * numberTwo
-}
-
-module.exports = {
-    multiple
-}
--- index.test.js
+++
@@ -1,6 +0,0 @@
-const { multiple } = require("./index")
-
-test("add returns the sum of two numbers", () => {
-    const product = multiple(4, 5)
-    expect(product).toBe(20)
-})
